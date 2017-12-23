a0cdc39486cf5ec04399f5d6c6330a7cce01ee1e
Kevin Albertson <kevin.albertson@10gen.com>
2017-12-23 15:06:24 -0500

    silliness

--- main.c
+++ main.c
@@ -18,45 +18,130 @@
 #include <fuse.h>
 #include <mongoc.h>
 
+typedef struct {
+   bool exists;
+   bool is_bad;
+   bool is_dir;
+   char id[25];
+   int skips;
+} parsed_path_t;
+
+parsed_path_t parse_path(const char* path) {
+   const char* path_iter = path;
+   parsed_path_t parsed = {0};
+   while (strstr(path_iter, "/it") != NULL) {
+      parsed.skips++;
+      path_iter += 3;
+   }
+
+   // If this is a directory, the rest should be a slash.
+   int remaining = strlen(path_iter);
+   if (remaining == 1 && path_iter[0] == '/') {
+      parsed.exists = true;
+      parsed.is_bad = false;
+      parsed.is_dir = true;
+   }
+   else if (remaining == 24 + 1) {
+      // TODO: query to check for existence
+      parsed.exists = true;
+      parsed.is_bad = false;
+      parsed.is_dir = false;
+      strncpy(parsed.id, path_iter + 1, 24);
+      parsed.id[24] = '\0';
+   } else {
+      parsed.exists = false;
+      parsed.is_bad = true;
+      parsed.is_dir = false;
+   }
+   return parsed;
+}
+
 // fuse may run in multi-threaded mode.
 // TODO: I may need to (a) enforce single threaded or (b) use client_pool
 mongoc_client_t* client;
 #define FUSE_USE_VERSION 26
 
-static const char *hello_str = "Hello World!\n";
-static const char *hello_path = "/huh";
-
 // number of documents to be shown in a directory. -1 for all
 static int batch_size = 1000;
 
-static int mongofs_getattr(const char *path, struct stat *stbuf)
-{
-   int res = 0;
-
-   memset(stbuf, 0, sizeof(struct stat));
-
-   bool is_dir = false;
-   const char* path_iter = path;
-   while (strstr(path_iter, "/it") != NULL) {
-      path_iter += 3;
+// must free
+int get_bson_string(const char* id, char** out, int* outlen) {
+   int exit_status = 0;
+   if (!bson_oid_is_valid(id, 24)) {
+      printf("malformed oid\n");
+      return -ENOENT;
    }
 
-   if (strlen(path_iter) == 0 || path_iter[0] != '/')
+   *out = NULL;
+
+   bson_oid_t oid;
+   bson_oid_init_from_string (&oid, id);
+
+   bson_t query;
+   bson_init(&query);
+   bson_append_oid(&query, "_id", 3, &oid);
+
+   bson_t opts;
+   bson_init(&opts);
+
+   mongoc_collection_t* coll = mongoc_client_get_collection (client, "test", "coll");
+   mongoc_cursor_t* cursor = mongoc_collection_find_with_opts (coll, &query, &opts, NULL);
+
+   bson_destroy (&query);
+   bson_destroy (&opts);
+
+   bson_error_t err;
+
+   const bson_t *out_doc;
+   if (mongoc_cursor_next(cursor, &out_doc)) {
+      *out = bson_as_json(out_doc, (size_t*)outlen);
+   }
+   else if (mongoc_cursor_error(cursor, &err)) {
+      exit_status = -EIO;
+      goto cleanup;
+   }
+   else {
+      printf("doc with id %s not found\n", id);
+      exit_status = -ENOENT;
+      goto cleanup;
+   }
+
+cleanup:
+   mongoc_collection_destroy(coll);
+   mongoc_cursor_destroy(cursor);
+   return exit_status;
+}
+
+static int mongofs_getattr(const char *path, struct stat *stbuf)
+{
+   printf("getattr %s \n", path);
+   memset(stbuf, 0, sizeof(struct stat));
+
+   parsed_path_t parsed = parse_path(path);
+
+   if (parsed.is_bad || !parsed.exists)
       return -ENOENT;
 
-   ++path_iter; // skip slash
-   is_dir = strlen(path_iter) == 0;
+   // is marking .localized a directory causing a hang?
 
-   if (is_dir == 0) {
+   if (parsed.is_dir) {
       stbuf->st_mode = S_IFDIR | 0755;
       stbuf->st_nlink = 2;
    } else {
+      char* bson_str = "";
+      printf("what is the address of bson_str? %x\n", &bson_str);
+      int bson_str_len;
+      int exit_status = get_bson_string (parsed.id, &bson_str, &bson_str_len);
+      if (exit_status != 0) return exit_status;
       stbuf->st_mode = S_IFREG | 0444;
       stbuf->st_nlink = 1;
       // stbuf->st_size = strlen(hello_str); not sure if this matters for reading
-      stbuf->st_size = 0;
+      stbuf->st_size = bson_str_len; // `cat` will read based off of size.
+      bson_free(bson_str);
+      printf("done\n");
    }
-   return res;
+
+   return 0;
 }
 
 static int mongofs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
@@ -66,6 +151,9 @@ static int mongofs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
    // (void) offset;
    // (void) fi;
 
+   printf("readdir\n");
+   //return 0;
+
    filler(buf, ".", NULL, 0);
    filler(buf, "..", NULL, 0);
 
@@ -73,26 +161,19 @@ static int mongofs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
    bson_t empty = BSON_INITIALIZER;
 
    int exit_status = 0; // the error code the function returns with
-   int skip = 0;
 
-   // parse path to see how many "it" directories down we are.
-   const char* path_iter = path;
-   while (strstr(path_iter, "/it") != NULL) {
-      path_iter += 3;
-      skip++;
-   }
+   parsed_path_t parsed = parse_path(path);
 
-   // TODO: confirm, but I believe we should always have a trailing slash.
-   if (strlen(path_iter) == 0 || path_iter[0] != '/')
+   if (parsed.is_bad || !parsed.is_dir)
       return -ENOENT;   
 
    bson_t* opts = BCON_NEW(
-      "skip", BCON_INT64(skip * batch_size),
+      "skip", BCON_INT64(parsed.skips * batch_size),
       "limit", BCON_INT32(batch_size),
       "projection", "{", "_id", BCON_INT32(1), "}");
 
    mongoc_collection_t* coll = mongoc_client_get_collection (client, "test", "coll");
-   mongoc_cursor_t* cursor = mongoc_collection_find_with_opts(coll, &empty, opts, NULL /* read pref */);
+   mongoc_cursor_t* cursor = mongoc_collection_find_with_opts(coll, &empty, opts, NULL );
 
    bson_free(opts);
    
@@ -108,7 +189,8 @@ static int mongofs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
          // TODO: filler may return 1 if buf is full. Instead, change this to use the offset form
          // of readdir so filled buffer won't break.
          int ret = filler(buf, asStr + 19, NULL, 0);
-         bson_free(asStr);
+         printf("adding %s\n", asStr + 19);
+         //bson_free(asStr);
          if (ret != 0) {
             exit_status = -EIO;
             goto cleanup;
@@ -132,6 +214,12 @@ cleanup:
 static int mongofs_open(const char *path, struct fuse_file_info *fi)
 {
    // see `man 2 open` for flags
+
+   parsed_path_t parsed = parse_path(path);
+
+   if (parsed.is_bad || !parsed.exists)
+      return -ENOENT;
+
    if ((fi->flags & 3) != O_RDONLY)
       return -EACCES;
 
@@ -145,71 +233,30 @@ static int mongofs_open(const char *path, struct fuse_file_info *fi)
 static int mongofs_read(const char *path, char *buf, size_t size, off_t offset,
                       struct fuse_file_info *fi)
 {
+   printf("reading %s\n", path);
    (void) fi;
-   int exit_status;
    // path should be of the form /it/it/it/....<id>
-   const char* path_iter = path;
-   while (strstr(path_iter, "/it") != NULL) {
-      path_iter += 3;
-   }
+   parsed_path_t parsed = parse_path(path);
 
-   if (strlen(path_iter) != 1 + 24) {
-      printf("malformed read, expecting file to be /<24 chars>\n");
+   if (parsed.is_bad || !parsed.exists) {
       return -ENOENT;
    }
 
-   ++path_iter; // skip slash
+   char* as_str;
+   int length;
+   int exit_status = get_bson_string(parsed.id, &as_str, &length);
 
-   if (!bson_oid_is_valid(path_iter, 24)) {
-      printf("malformed oid\n");
-      return -ENOENT;
+   if (exit_status != 0) {
+      return exit_status;
    }
 
-   bson_oid_t oid;
-   bson_oid_init_from_string (&oid, path_iter);
-
-   bson_t query;
-   bson_init(&query);
-   bson_append_oid(&query, "_id", 3, &oid);
-
-   bson_t opts;
-   bson_init(&opts);
-
-   mongoc_collection_t* coll = mongoc_client_get_collection (client, "test", "coll");
-   mongoc_cursor_t* cursor = mongoc_collection_find_with_opts (coll, &query, &opts, NULL);
-
-   bson_destroy (&query);
-   bson_destroy (&opts);
-
-   bson_error_t err;
-
-   const bson_t *out;
-   if (mongoc_cursor_next(cursor, &out)) {
-      // instead of copying, I could hack/write my own visitor to write in place.
-      int length;
-      char* doc_data = bson_as_json(out, &length);
-      int bytes_to_copy = size;
-      if (offset + size > length) {
-         // this is the last read.
-         bytes_to_copy = length - offset; // might be zero
-      }
-      memcpy(buf, doc_data + offset, bytes_to_copy);
-      return bytes_to_copy;
+   int bytes_to_copy = size;
+   if (offset + size > length) {
+      // this is the last read.
+      bytes_to_copy = length - offset; // might be zero
    }
-   else if (mongoc_cursor_error(cursor, &err)) {
-      exit_status = -EIO;
-      goto cleanup;
-   }
-   else {
-      printf("doc with id %s not found\n", path_iter);
-      exit_status = -ENOENT;
-      goto cleanup;
-   }
-
-cleanup:
-   mongoc_collection_destroy(coll);
-   mongoc_cursor_destroy(cursor);
-   return exit_status;
+   memcpy(buf, as_str + offset, bytes_to_copy);
+   return bytes_to_copy;
 }
 
 static struct fuse_operations mongofs_oper = {
@@ -223,7 +270,7 @@ int main(int argc, char *argv[])
 {
    mongoc_init();
    client = mongoc_client_new("mongodb://localhost:27017");
-
+   printf("testing before fuse_main\n");
    return fuse_main(argc, argv, &mongofs_oper, NULL);
 
 }
